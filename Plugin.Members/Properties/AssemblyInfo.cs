eec98abf787f7ff883a88dbc773046384046b1a4
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 03:35:16 +0300

    Unify plugins deployment

--- Plugin.Members/Properties/AssemblyInfo.cs
+++ Plugin.Members/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("dbd0389f-bd3c-45ef-81f7-bcee9d99d7c4")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,15 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=98")]
 #else
 
-[assembly: AssemblyTitle("Plugin.Members")]
 [assembly: AssemblyDescription("Show loaded plugins public methods and properties")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.Members")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
 #endif
 
